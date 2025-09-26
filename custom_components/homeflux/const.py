9413e7cb211455572a5d2eea629777aa0483adb3
vinxion <onnovangijssel@hotmail.com>
2025-09-26 11:37:50 +0200

    Create const.py

---
+++ custom_components/homeflux/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "homeflux"
+
+CONF_ENDPOINT = "endpoint"
+CONF_TOKEN = "token"
+CONF_GRID_ENTITY = "grid_entity"
+CONF_PV_ENTITY = "pv_entity"
+CONF_INTERVAL = "interval"
+
+# Hardcoded endpoint voor productie
+DEFAULT_ENDPOINT = "https://energy-poc.onnovangijssel.workers.dev"
+DEFAULT_INTERVAL = 60  # seconden
