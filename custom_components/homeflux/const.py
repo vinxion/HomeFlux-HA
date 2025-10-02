e7d5fbd3aef1c922f5d041269f1071054d364613
vinxion <onnovangijssel@hotmail.com>
2025-10-02 11:37:55 +0200

    Update const.py

--- custom_components/homeflux/const.py
+++ custom_components/homeflux/const.py
@@ -6,6 +6,12 @@ CONF_GRID_ENTITY = "grid_entity"
 CONF_PV_ENTITY = "pv_entity"
 CONF_INTERVAL = "interval"
 
-# Endpoint voor productie
+# Cumulatieve energie-entiteiten (kWh, total_increasing)
+CONF_GRID_IMPORT_TOTAL_ENTITY = "grid_import_total_entity"
+CONF_GRID_EXPORT_TOTAL_ENTITY = "grid_export_total_entity"
+CONF_PV_TOTAL_ENTITY = "pv_total_entity"
+
+# Defaults
 DEFAULT_ENDPOINT = "https://api.homeflux.io"
+INGEST_PATH = "/ingest"   
 DEFAULT_INTERVAL = 60  # seconden
